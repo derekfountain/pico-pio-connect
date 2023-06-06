83248c716a1733d1e8b3e9b3a41d9ecc9a5bb9ec
Derek Fountain <derek@scratters.com>
2023-06-06 13:17:10 +0100

    add example 2

--- example2/pico_1/pico_1.c
+++ example2/pico_1/pico_1.c
@@ -1,85 +1,33 @@
-#include <ctype.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+/*
+ * Pico PIO Connect, a utility to connect Raspberry Pi Picos together
+ * Copyright (C) 2023 Andrew Menadue
+ * Copyright (C) 2023 Derek Fountain
+ * 
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
 #include "hardware/clocks.h"
 #include "hardware/gpio.h"
-#include "hardware/vreg.h"
-#include "pico/multicore.h"
 #include "picoputer.pio.h"
 
-const uint8_t  TEST_OUTPUT_GP           = 0;
+const uint8_t LED_PIN         = PICO_DEFAULT_LED_PIN;
 
-const uint8_t LED_PIN = PICO_DEFAULT_LED_PIN;
-
-#include "../common/link_common.h"
-
-// The PIOs and SMs of the link with the other Pico
-int linkout_sm;
-PIO linkout_pio;
-int linkin_sm;
-PIO linkin_pio;
-
-// Use this if breakpoints don't work
-#define DEBUG_STOP {volatile int x = 1; while(x) {} }
-
-const int LINKOUT_PIN  = 15;
-const int LINKIN_PIN   = 14;
-
-const int CLOCK_PIN    = 13;
-
-void comfn_send_ram_data_done(void)
-{
-}
-
-
-static uint8_t message[] = "Hello, world from Pico 1\n";
-void send_ram_data(void)
-{
-  printf("\nSending message...");
-  send_byte_string(message, comfn_send_ram_data_done);
-}
-
-
-void lfsm_ack_received(void)
-{
-  printf("\nFSM setting IDLE after ACK...");
-  link_fsm_stimulus(LFSM_SEND_IDLE, 0);
-}
-
-
-
-BYTE idle_command[2] = { 1, LINK_CMD_IDLE};
-BYTE test_int_command[3] = { 2, LINK_CMD_TEST_INT, 171};
-
-void comfn_idle_done(void)
-{
-  for( int i=0; i<5; i++ )
-  {
-    sleep_ms( 100 );
-    gpio_put( LED_PIN, 1 );
-    sleep_ms( 100 );
-    gpio_put( LED_PIN, 0 );
-  }
-
-  printf("\nIdle command sent\n");
-}
-
-void comfn_test_int_done(void)
-{
-  // Send the next test int
-  //printf("\nIdle command sent\n");
-  //  sleep_us(100);
-  send_byte_string(test_int_command, comfn_test_int_done);
-  test_int_command[2]++;
-  if( test_int_command[2] == 0 )
-    {
-      test_int_command[2]++;
-    }
-}
-
+const uint8_t TEST_OUTPUT_GP  = 0;
+const uint8_t LINKOUT_PIN     = 15;
+const uint8_t LINKIN_PIN      = 14;
 
 static void __time_critical_func(blip_test_pin)( void )
 {
@@ -95,82 +43,93 @@ static void __time_critical_func(blip_test_pin)( void )
   __asm volatile ("nop");
 }
 
+static int linkin_sm;
+
+uint8_t receive_byte( void )
+{
+  /* Read from PIO input FIFO */
+  uint32_t data = picoputerlinkin_get(pio0, linkin_sm);
+
+  /* Invert what's been received */
+  data = data ^ 0xFFFFFFFF;
+
+  /* It arrives from the PIO at the top end of the word, so shift down */
+  data >>= 22;
+      
+  /* Remove stop bit in LSB */
+  data >>= 1;
+	  
+  /* Mask out data, just to be sure */
+  data &= 0xff;
+
+  return (uint8_t)data;
+}
+
 int main()
 {
-
-  // DEBUG_STOP;
-
   gpio_init( LED_PIN ); gpio_set_dir( LED_PIN, GPIO_OUT );
   
-  stdio_init_all();  
-  sleep_ms(2000);
-
-  printf("\n\n");
-  printf("\n/------------------------------\\");
-  printf("\n| Pico 1                       |");
-  printf("\n/------------------------------/");
-  printf("\n");
-  
-  printf("\nSetting PIOs...");
-  
-  // Set the link to the other Pico up
-  gpio_init(LINKOUT_PIN);
-  gpio_set_dir(LINKOUT_PIN,GPIO_OUT);
-  gpio_put(LINKOUT_PIN, 1);
-  gpio_set_function(LINKOUT_PIN, GPIO_FUNC_PIO0);
-  
-  gpio_init(LINKIN_PIN);
-  gpio_set_dir(LINKIN_PIN,GPIO_IN);
-  gpio_set_function(LINKIN_PIN, GPIO_FUNC_PIO0);
-
   /* Test pin, blips the scope */
   gpio_init(TEST_OUTPUT_GP); gpio_set_dir(TEST_OUTPUT_GP, GPIO_OUT);
   gpio_put(TEST_OUTPUT_GP, 0);
 
-  //--------------------------------------------------------------------------------
-  // Set up link out state machine
-  int sm;
-  PIO pio;
+  /* Set up the link to the other Pico */
+  gpio_init(LINKOUT_PIN); gpio_set_dir(LINKOUT_PIN,GPIO_OUT); gpio_put(LINKOUT_PIN, 1);
+  gpio_set_function(LINKOUT_PIN, GPIO_FUNC_PIO0);
 
-  pio=pio0;
-  
-  sm = pio_claim_unused_sm(pio, true);
-
-  linkout_sm = sm;
-  linkout_pio = pio;
-  
-  uint offset = pio_add_program(pio, &picoputerlinkout_program);
-  picoputerlinkout_program_init(pio, sm, offset, LINKOUT_PIN);
-
-  sm = pio_claim_unused_sm(pio, true);
-  
-  linkin_sm = sm;
-  linkin_pio = pio;
-  offset = pio_add_program(pio, &picoputerlinkin_program);
-  picoputerlinkin_program_init(pio, sm, offset, LINKIN_PIN);
-  
+  gpio_init(LINKIN_PIN); gpio_set_dir(LINKIN_PIN,GPIO_IN);
+  gpio_set_function(LINKIN_PIN, GPIO_FUNC_PIO0);
     
+  /* Outgoing side of the link */
+  int linkout_sm  = pio_claim_unused_sm(pio0, true);
+  uint offset     = pio_add_program(pio0, &picoputerlinkout_program);
+  picoputerlinkout_program_init(pio0, linkout_sm, offset, LINKOUT_PIN);
+
+  /* Incoming side of the link */
+  linkin_sm       = pio_claim_unused_sm(pio0, true);
+  offset          = pio_add_program(pio0, &picoputerlinkin_program);
+  picoputerlinkin_program_init(pio0, linkin_sm, offset, LINKIN_PIN);
+
+  /*
+   * Ping a data byte back and forth. This side sends a 0xDF, then
+   * sits waiting for the other side to respond with 0xFD. Repeats.
+   *
+   * You should see 2 LED blinks, then 1 LED blink, repeatedly.
+   */
+  uint8_t sending = 1;
+
   while(1)
   {
     sleep_ms(1000);
 
-    gpio_put( LED_PIN, 1 );
-    sleep_ms(50);
-    gpio_put( LED_PIN, 0 );
+    if( sending )
+    {
+      gpio_put( LED_PIN, 1 );
+      sleep_ms(50);
+      gpio_put( LED_PIN, 0 );
+      sleep_ms(50);
+      gpio_put( LED_PIN, 1 );
+      sleep_ms(50);
+      gpio_put( LED_PIN, 0 );
 
-//    blip_test_pin();
-//    blip_test_pin();
-//    blip_test_pin();
-//    blip_test_pin();
-//    blip_test_pin();
-    send_byte_to_link( 0xDF );
-//    blip_test_pin();
-//    blip_test_pin();
+      const uint32_t data = 0xDF;
+      pio_sm_put_blocking(pio0, linkout_sm, 0x200 | ((data ^ 0xff)<<1));
 
-//    send_byte_string(idle_command, comfn_idle_done);
-//    send_byte_string(test_int_command, comfn_test_int_done);
-//    test_int_command[2]++;
+      sending = 0;
+    }
+    else
+    {
+      uint8_t data = receive_byte();
 
-//    link_process();
+      if( data == 0xFD )
+      {
+	gpio_put( LED_PIN, 1 );
+	sleep_ms(50);
+	gpio_put( LED_PIN, 0 );
+      }
+
+      sending = 1;
+    }
+
   }
 }
